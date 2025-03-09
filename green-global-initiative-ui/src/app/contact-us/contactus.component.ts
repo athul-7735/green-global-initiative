2c2ca17c856bd92b7abf6dc35a154cea3d6ceae7
Siddhartha-atu <l00187620@atu.ie>
2025-03-09 23:18:54 +0000

    Feat(Development): #NGGI-102 Integration of contact us page

--- green-global-initiative-ui/src/app/contact-us/contactus.component.ts
+++ green-global-initiative-ui/src/app/contact-us/contactus.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import { ContactusService } from './services/contactus.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../authentication/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [MatIconModule],
+  imports: [MatIconModule, FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './contactus.component.html',
   styleUrl: './contactus.component.scss'
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit {
 
+  contactUsForm: FormGroup;
+
+  constructor(private contact: ContactusService, private fb: FormBuilder, private authenticationService: AuthService, private toastr: ToastrService) { 
+    this.contactUsForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required]], 
+      phone: ['', [Validators.required]],
+      message: ['', [Validators.required]]
+    })
+  }
+
+  ngOnInit() {
+    
+    }
+
+    onSubmit() {
+      this.authenticationService.removeItem();
+      let requestObject = this.contactUsForm.value;
+      this.contact.postQuery('contact-us', requestObject).subscribe((response) => {
+        console.log('Query Submitted!', response);
+        this.toastr.success('Query Submitted Successfully', 'Success',  {
+          progressBar: true, closeButton: true 
+        });
+      },(err)=>{
+        console.log(err);
+        this.toastr.error('Invalid Query!', 'Error',  {
+          progressBar: true, closeButton: true 
+        });
+      });
+      this.contactUsForm.reset();
+    }
 }
